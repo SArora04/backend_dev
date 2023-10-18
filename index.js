af87e6cd29ea035cab92313e925d97e2f6e4bc79
SArora04 <sanyarora515@gmail.com>
2023-10-18 15:44:44 +0530

    root user changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express() // app is express variable been decalred here
 const port = 3000      // port we're using in order to run this - while this is a virtual port 
 
 app.get('/', (req, res) => {        //sending a request to '/'- home route through app 
-  res.send('Hello World!')          // response of the request
+  res.send('Hello Sanyaaaa!')          // response of the request
 })
 
 app.get('/instagram', (req,res) => {
